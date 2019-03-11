d8dfc9eb51f5c45aa8476129e5b66e704649fba1
kari89diaz <kari89diaz@gmail.com>
2019-03-11 10:24:20 -0300

    Insert a number to know if it's pair or impair

---
+++ IfPair/IfPair/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            Console.WriteLine("Insert number");
+            num = int.Parse(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                Console.WriteLine(num + " Es par");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine(num + " Es impar");
+                Console.ReadKey();
+            }
+        }
+    }
+}
